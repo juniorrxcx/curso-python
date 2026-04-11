d719e8ca2a94408e27b3c5c2879415d78d0cbcf4
juniorrxcx <juniorrxcx13@icloud.com>
2026-04-11 18:38:58 -0300

    Aula 12: exercício cálculo de IMC

---
+++ aula12.py
@@ -0,0 +1,8 @@
+# IMC = peso / (altura * altura)
+nome = "Junior Lima"
+altura = 1.73
+peso = 60
+imc = peso // (altura ** 2)
+
+print(nome, "tem ", altura, "de altura\n"\
+      "pesa", peso, "quilos e seu IMC é", imc )
