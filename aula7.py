a31dc12b479be597f589b03f027eb781d3d6a3a8
juniorrxcx <juniorrxcx13@icloud.com>
2026-04-11 17:50:01 -0300

    Aula 7: introdução de variáveis e PEP8

---
+++ aula7.py
@@ -0,0 +1,14 @@
+# PEP8: inicie variáveis com letras minúsculas, pode usar numeros e underline
+# nome_completo = 'Junior Lima'
+# soma_dois_mais_dois = 2 + 2
+# bool_um = bool('1')
+# print(bool_um, type(bool_um))
+# print(nome_completo, soma_dois_mais_dois)
+# print(soma_dois_mais_dois)
+
+
+nome = "Junior"
+idade = 21
+maior_de_idade = idade >= 18
+print('Nome:', nome, "Idade:", idade)
+print("É maior de idade?", maior_de_idade)
