ba7088dfccb50deebfda813f65ddf5b015b5eb44
juniorrxcx <juniorrxcx13@icloud.com>
2026-04-11 16:23:14 -0300

    Aula 1: comentários e DocString
    Aula 2: comando print

---
+++ aula1.py
@@ -0,0 +1,13 @@
+'''
+DocString
+Escreve o que quiser
+mas nao é comentário
+
+Utiliza 3 aspas simples ou duplas
+para abertura e fechamento
+'''
+
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(123)
