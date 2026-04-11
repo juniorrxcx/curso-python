97e2baf3a27eebea0326f5f00e8df46742027f6c
juniorrxcx <juniorrxcx13@icloud.com>
2026-04-11 16:46:29 -0300

    Aula 4: função type

---
+++ aula4.py
@@ -0,0 +1,12 @@
+print(11) # int
+print(-11) # int
+print (0)
+
+print(1.1, -10.11) # float (separado por .)
+
+# classe type mostra o tipo que o python inferiu ao valor
+print(type("Junior"))
+print(type(-1))
+print(type(1.1), type(-1.1), type(0.0), type(0))
+
+# tudo em python é um objeto
