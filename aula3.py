a67f678d75c6366f66956277ef2b60a5e6efcbfe
juniorrxcx <juniorrxcx13@icloud.com>
2026-04-11 16:37:55 -0300

    Aula 3: print com strings e escapes

---
+++ aula3.py
@@ -0,0 +1,12 @@
+print(1234)
+# String com aspas e aspas duplas
+print('Junior Lima')
+print(1, 'Junior "Lima"')
+print("Junior Lima")
+print(2, "Junior 'Lima'")
+
+# Escape (ignora e mostra o proximo caractere)
+print("Junior \"Lima\"")
+
+# r (mostra até o escape)
+print(r"Junior \"Lima\"")
