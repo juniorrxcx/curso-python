b5bb9c7ac2aa6fa01075b2b0bd990852f3f65dec
juniorrxcx <juniorrxcx13@icloud.com>
2026-04-11 18:31:18 -0300

    Aulas 10 e 11: concatenação(+), repetição(*) e precedência entre os op aritiméticos

---
+++ aula10.py
@@ -0,0 +1,7 @@
+concatenacao =  'A' + ' ' + 'B'
+print(concatenacao)
+
+a_dez_vezes = "A" * 10
+tres_vezes_junior = 3 * "Junior"
+print(a_dez_vezes)
+print(tres_vezes_junior)
---
+++ aula11.py
@@ -0,0 +1,17 @@
+'''
+Ordem de execução:
+
+#1 Parênteses internos primeiro
+#2 Potenciacao ou exponenciacao
+#3 Divisão ou módulo(resto da divisao)
+#4 Soma e subtração
+
+Matemática básica
+
+'''
+
+conta_1 = (1 + (0.5 + 0.5)) ** (5 + 5)
+print(conta_1)
+# Posso trocar o valor da variável durante a execução do programa
+conta_1 = 'Qualquer coisa'
+print(conta_1)
