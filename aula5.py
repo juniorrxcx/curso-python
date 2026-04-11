0b45ac51af6cd6734aa0643137464332c36cdc1e
juniorrxcx <juniorrxcx13@icloud.com>
2026-04-11 16:52:14 -0300

    Aula 5: dado booleano

---
+++ aula5.py
@@ -0,0 +1,8 @@
+# Tipo de dado bool (boolean)
+# sim (true) não (false)
+# == questiona se um valor é igual a outro
+print(10 == 10) # Sim => True (verdadeiro)
+print(10 == 11) # Não => False (falso)
+print(type(10 == 11))
+print(type(True))
+print(type(False))
