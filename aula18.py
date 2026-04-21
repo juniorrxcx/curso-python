7d2856f3219134fabb9a82760d68505ccfd75875
juniorrxcx <juniorrxcx13@icloud.com>
2026-04-20 21:29:32 -0300

    Aula 18: python debugger em if/elif/else

---
+++ aula18.py
@@ -0,0 +1,23 @@
+condicao1 = False
+condicao2 = True
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('Codigo para a condicao 1')
+    print('Codigo para a condicao 1')
+elif condicao2:
+    print('Codigo para a condicao 2')
+elif condicao3:
+    print('Codigo para a condicao 3')    
+elif condicao4:
+    print('Codigo para a condicao 4')
+else:
+    print('Este é o else do primeiro if')
+
+
+
+if 10 == 10:
+    print('Sim 10 = 10 ')
+
+print('Fora do if')
