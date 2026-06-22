ee995470d1490bfcfcd93859ffef647a86693d37
juniorrxcx <juniorrxcx13@icloud.com>
2026-06-22 19:35:44 -0300

    Aulas 34 a 36: exemplos de loops while e operadores de atribuição

---
+++ aula36.py
@@ -0,0 +1,14 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+
+contador = 1
+
+# while contador < 10:
+#     contador = contador + 1
+#     print(contador)
+
+# print('Acabou')
+contador += 2
+print(contador)
