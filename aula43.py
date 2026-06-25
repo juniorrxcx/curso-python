ea467cdd110cfcd6fbc5e2d41affbc4396eb2071
juniorrxcx <juniorrxcx13@icloud.com>
2026-06-25 19:12:17 -0300

    Aulas 43 e 44: for e range

---
+++ aula43.py
@@ -0,0 +1,7 @@
+texto = 'Python'
+
+#para cada letra em texto, exiba a letra
+#passa letra por letra
+
+for letra in (texto):
+    print(letra)
---
+++ aula44.py
@@ -0,0 +1,7 @@
+#todos os numeros pares de 0 a 100 de 2 em 2
+numeros = range(0, 100, 2)
+print(numeros)
+
+#para cada numero em numeros
+for numero in numeros:
+    print(numero)
